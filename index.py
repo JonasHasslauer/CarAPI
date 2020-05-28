82395da7ec1ca7c2afea60a800aa45116cff115f
JonasHasslauer <jonashasslauer@gmail.com>
2020-05-29 00:37:50 +0200

    - deleted test statements - added lines to open url and read the page

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
-# test
-# test 2
+from bs4 import BeautifulSoup
+import requests
+import json
+import html
+
+marken = 'https://www.kfz-auskunft.de/autohersteller.html'
+soup = BeautifulSoup(content.text, 'html.parser')
